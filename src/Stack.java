33612f5def92fda393e566c491cbde665e264b99
lukeodrobinak <ekul.luke12@gmail.com>
2023-11-14 10:54:55 -0700

    First push with basic functions of StackNode

---
+++ src/Stack.java
@@ -0,0 +1,18 @@
+/**
+ * Stack.java
+ * Authors: Luke O’Drobinak
+ * Date: 10/5/2023
+ * Collaborators:
+ * Citations: Google, Oracle Java Docs, GitHub Documentation, Stack Overflow
+ **/
+
+class Stack {
+
+    private int size;
+    private Node<T> rootNode;
+
+    public Stack() {}               //Default constructor
+
+    public void push();
+
+}
